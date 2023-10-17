49aed48d0fc46e1c3d4638f8f44d1f792a7c24b5
AllisonDev <allisonsilveiradev@gmail.com>
2023-10-17 19:27:01 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,49 @@
-import { MenuIcon, ShoppingCart } from "lucide-react";
+import {
+  MenuIcon,
+  ShoppingCart,
+  LogInIcon,
+  BadgePercent,
+  ListOrdered,
+  Home,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between p-[1.8rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-3">
+            <Button variant="outline" className="w-full gap-2">
+              <LogInIcon />
+              Fazer Login
+            </Button>
+            <Button variant="outline" className="w-full gap-2">
+              <Home />
+              Início
+            </Button>
+            <Button variant="outline" className="w-full gap-2">
+              <BadgePercent />
+              Ofertas
+            </Button>
+            <Button variant="outline" className="w-full gap-2">
+              <ListOrdered />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold ">
         <span className="text-primary">AllisonDev</span>Store
