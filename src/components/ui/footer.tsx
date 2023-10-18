a8f7f9f6651d62a985dde42d33036c4038cb2068
AllisonDev <allisonsilveiradev@gmail.com>
2023-10-18 19:02:17 -0300

    feat: add sections and footer in home

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.8rem]">
+      © 2023 Copyright <span className="font-semibold">AllisonDev Store</span>
+    </div>
+  );
+};
+
+export default Footer;
